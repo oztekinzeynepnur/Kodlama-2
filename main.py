a248526dd0980513933e3839d3ec5dbab06908d9
JrRobdelirdi <>
2022-11-30 12:19:36 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+yas=int((input("yaşınısöyle")))
+if yas<=13:
+  print("çocuksun")
+if 13<yas<=28
+  print("gençsin")
+if 28<yas<=40:
+  print("orta yaşlısın")
+if yas>40:
+  prınt("yaşlısın")
+
+  
